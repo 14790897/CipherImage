e5f8209007472b2785aadd48ed45f4c438a77b65
liuweiqing <liuweiqing147@gmail.com>
2025-06-09 14:21:44 +0800

    ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
